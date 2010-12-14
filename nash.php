c19b3b075396cc439d7e13be1af49200fb6930b6
Guy Halford-Thompson <mrwooster@gmail.com>
2010-12-14 21:21:38 +0000

    First working release

--- nash.php
+++ nash.php
@@ -4,19 +4,47 @@
  */
 
 // Determine year, month, day and hour
-$year  = date('Y');
-$month = date('m');
-$day   = date('d');
-$hour  = date('H');
+$year       = date('Y');
+$month      = date('m');
+$day        = date('d');
+$hour       = date('H');
+$yesterday  = date('d', time() - 24*60*60);
 
-echo $month." - ".$day." - ".$hour;
+// Load configs
+$config = parse_ini_file('nash.ini');
 
 // Build hourly file file name
-$fname = 'Hourly-'.$hour.'.sql';
+$fnameHourly = 'Hourly-'.$hour.'.sql';
+$fnameDaily = 'Daily-'.$year.'-'.$month.'-'.$day.'-'.$hour.'.sql';
+$basedirHourly = 'hourly';
+$basedirDaily  = 'daily';
+
+// Make todays directory if it does not exist
+if (!is_dir($basedirHourly.'/'.$day)) {
+	mkdir($basedirHourly.'/'.$day, 0744);
+}
+
+// Remove yesterdays directory if it exists
+if (is_dir($basedirHourly.'/'.$yesterday))
+{
+	// Directory exists so delete everything in it
+	foreach (glob($basedirHourly.'/'.$yesterday.'/*') as $filename) {
+		unlink($filename);
+	}
+
+	// Now delete the directory
+	rmdir($basedirHourly.'/'.$yesterday);
+}
 
 // Create backup
-$out = array();
-$cmd = 'add command here';
-exec($cmd, $out);
+$cmd = 'mysqldump '.$config['database'].' -u '.$config['username'].' -p'.$config['password'];
+$path = $basedirHourly.'/'.$day.'/'.$fnameHourly;
+exec($cmd.' > '.$path);
+
+// Is this the daily backup
+if ($hour == $config['dailyBackupHour']) {
+	$path = $basedirDaily.'/'.$fnameDaily;
+	exec($cmd.' > '.$path);
+}
 
 ?>
